b02fbcdf9d5a1c9401e333bfdcf4d6777c8ed015
Nicholas Walkewicz <nwalkewicz@gmail.com>
2022-05-31 13:01:57 -0700

    Add Firestore key logic

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const server = express();
 
 const port = process.env.PORT || 3000;
+
+let fskey;
+try {
+	fskey = process.env.FIRESTORE_KEY || require(path.join(__dirname, '../private/firestore-key.json'));
+} catch(err) {
+	console.error(`Couldn't load Firestore key. Found error: `, err);
+}
 
 server.use(require('./routers/api'));
 server.use(express.static('public'));
